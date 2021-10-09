2f7620ce10cff2cf84cac3ac6b803ff9942b6166
taimoorkhan1122 <khant8k83@gmail.com>
2021-10-10 01:56:36 +0500

    added actions and types -- initial

---
+++ src/components/CakeCount.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CakeCount = () => {
+    return (
+        <div data-testid="cake-count">
+            <h2>Cake Counts</h2>
+            <button>Update count</button>
+        </div>
+    )
+}
+
+export default CakeCount
---
+++ src/components/CakeCount.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+
+import CakeCount from "./CakeCount";
+
+test("renders CakeCount Component", () => {
+  render(<CakeCount />);
+  const headiingElement = screen.getByText(/Cake Count/i);
+  expect(headiingElement).toBeInTheDocument();
+
+  const countButton = screen.getByRole("button");
+  expect(countButton.innerHTML).toContain("Update count");
+});
